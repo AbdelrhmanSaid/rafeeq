b53bb1960ad4b048c99d6170e023143ac88b91aa
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2024-08-17 04:39:23 +0300

    fix prayer times detect location

--- src/components/PrayerTimes.vue
+++ src/components/PrayerTimes.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { computed } from 'vue'
 import { useCoordinatesStore } from '@/stores/coordinates'
 import { useFetch, useDateFormat } from '@vueuse/core'
 
@@ -13,51 +14,64 @@ const timingsMap = {
   Isha: 'العشاء',
 }
 
+// Get the coordinates store
 const store = useCoordinatesStore()
 
-if (store.latitude != 0 && store.longitude != 0) {
-  const today = `${new Date().getDate()}-${new Date().getMonth() + 1}-${new Date().getFullYear()}`
-  const endpoint = `https://api.aladhan.com/v1/timings/${today}?latitude=${store.latitude}&longitude=${store.longitude}&iso8601=true`
+// Get the API endpoint
+const endpoint = computed(() => {
+  if (store.latitude == 0 || store.longitude == 0) {
+    return null
+  }
 
-  const { isFetching, data: timings, error } = useFetch(endpoint).json().get()
+  // Build the API endpoint using the current date
+  const today = `${new Date().getDate()}-${new Date().getMonth() + 1}-${new Date().getFullYear()}`
+  const url = `https://api.aladhan.com/v1/timings/${today}?latitude=${store.latitude}&longitude=${store.longitude}&iso8601=true`
+
+  return url
+})
+
+const options = {
+  refetch: true,
+  beforeFetch: ({ url, cancel }) => {
+    if (url == null) {
+      cancel()
+    }
+  },
 }
+
+// Start fetching the data
+const { isFetching, data: timings, error } = useFetch(endpoint, options).json().get()
 </script>
 
 <template>
-  <div v-if="store.latitude != 0 && store.longitude != 0">
-    <div class="message-box" v-if="isFetching">
-      <LoadingState />
-    </div>
-
-    <div class="message-box" v-else-if="error">
-      <ErrorState :code="500" message="حدث خطأ أثناء تحميل البيانات، برجاء المحاولة في وقت لاحق." />
-    </div>
-
-    <div class="timings" v-else-if="timings">
-      <div class="item" v-for="(timing, key) in timingsMap" :key="key">
-        <div class="item-title">{{ timing }}</div>
-        <div class="item-time">{{ useDateFormat(timings.data.timings[key], 'hh:mm A') }}</div>
-      </div>
-    </div>
+  <div
+    v-if="store.latitude == 0 || store.longitude == 0"
+    class="border rounded p-5 text-center cursor-pointer"
+    @click="store.detect()"
+  >
+    <span>إضغط هنا لتحديد الموقع الخاص بك وعرض مواقيت الصلاة</span>
   </div>
 
-  <div class="message-box cursor-pointer text-muted" @click="store.detect()" v-else>
-    <span>إضغط هنا لتحديد الموقع الخاص بك وعرض مواقيت الصلاة</span>
+  <div class="border rounded p-5" v-else-if="isFetching">
+    <LoadingState />
+  </div>
+
+  <div class="border rounded p-5" v-else-if="error">
+    <ErrorState :code="500" message="حدث خطأ أثناء تحميل البيانات، برجاء المحاولة في وقت لاحق." />
+  </div>
+
+  <div class="timings" v-else-if="timings">
+    <div class="item" v-for="(timing, key) in timingsMap" :key="key">
+      <div class="item-title">{{ timing }}</div>
+
+      <div class="item-time">
+        {{ useDateFormat(timings.data.timings[key], 'hh:mm A').value.replace('AM', 'ص').replace('PM', 'م') }}
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.message-box {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px dashed var(--bs-border-color);
-  border-radius: 0.25rem;
-  padding: 1.5rem 1rem;
-  min-height: 100px;
-  text-align: center;
-}
-
 .timings {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
@@ -79,7 +93,6 @@ if (store.latitude != 0 && store.longitude != 0) {
 
     .item-time {
       font-size: 1.5rem;
-      font-weight: 700;
     }
   }
 }
