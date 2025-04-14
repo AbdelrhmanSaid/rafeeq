2933054ffb8e3b936bc4d85da559659353623171
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2025-04-14 22:16:39 +0200

    fix: update fill color to currentColor for prayer icons in Asr, Fajr, Ishaa, Maghrib, and Shurooq components

--- src/components/icons/Prayers/Ishaa.vue
+++ src/components/icons/Prayers/Ishaa.vue
@@ -1,8 +1,8 @@
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" width="25" height="22.893" viewBox="0 0 25 22.893">
     <g transform="translate(-1055 -580.807)">
-      <rect width="25" height="1.7" rx="0.85" transform="translate(1055 598)" fill="#795547" />
-      <rect width="15" height="1.7" rx="0.85" transform="translate(1060 602)" fill="#795547" />
+      <rect width="25" height="1.7" rx="0.85" transform="translate(1055 598)" fill="currentColor" />
+      <rect width="15" height="1.7" rx="0.85" transform="translate(1060 602)" fill="currentColor" />
       <path
         fill="currentColor"
         d="M16.335,10.107a.73.73,0,0,0-.766-.1,5.874,5.874,0,0,1-2.459.533,5.947,5.947,0,0,1-5.939-5.91,6.268,6.268,0,0,1,.182-1.459.73.73,0,0,0-.97-.825A7.4,7.4,0,1,0,16.6,10.873.73.73,0,0,0,16.335,10.107ZM9.4,14.988A5.939,5.939,0,0,1,5.711,4.43v.2a7.406,7.406,0,0,0,7.4,7.4,7.143,7.143,0,0,0,1.532-.161A5.918,5.918,0,0,1,9.4,15.017Z"
