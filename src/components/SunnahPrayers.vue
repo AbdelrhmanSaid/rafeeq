900490032a0b000d2cbed7809837f25a5bd8e86e
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2025-04-14 22:13:45 +0200

    refactor: restructure table layout in SunnahPrayers component for improved readability

--- src/components/SunnahPrayers.vue
+++ src/components/SunnahPrayers.vue
@@ -29,21 +29,61 @@ const prayers = [
 </script>
 
 <template>
-  <table class="table table-bordered text-center">
+  <table class="table table-bordered">
     <thead>
       <tr>
-        <th scope="col">قبل</th>
-        <th scope="col">الصلاة</th>
-        <th scope="col">بعد</th>
+        <th></th>
+        <th v-for="(prayer, index) in prayers" :key="index">
+          {{ prayer.name }}
+        </th>
       </tr>
     </thead>
-
     <tbody>
-      <tr v-for="prayer in prayers" :key="prayer.name">
-        <td>{{ prayer.before }}</td>
-        <th scope="row">{{ prayer.name }}</th>
-        <td>{{ prayer.after }}</td>
+      <tr>
+        <th>قبل</th>
+        <td v-for="(prayer, index) in prayers" :key="index">
+          {{ prayer.before === '-' ? '-' : prayer.before === 2 ? 'ركعتان' : prayer.before + ' ركعات' }}
+        </td>
+      </tr>
+      <tr>
+        <th>بعد</th>
+        <td v-for="(prayer, index) in prayers" :key="index">
+          {{ prayer.after === '-' ? '-' : prayer.after === 2 ? 'ركعتان' : prayer.after + ' ركعات' }}
+        </td>
       </tr>
     </tbody>
   </table>
 </template>
+
+<style lang="scss" scoped>
+table {
+  outline: 1px solid var(--bs-border-color);
+  border-radius: var(--bs-border-radius);
+  overflow: hidden;
+
+  thead tr:first-child {
+    border-top: none !important;
+  }
+
+  tbody tr:last-child {
+    border-bottom: none !important;
+  }
+
+  th:first-child,
+  th:last-child,
+  td:first-child,
+  td:last-child {
+    border: none !important;
+  }
+
+  th,
+  td {
+    text-align: center;
+    vertical-align: middle;
+  }
+
+  th {
+    background-color: var(--bs-tertiary-bg);
+  }
+}
+</style>
