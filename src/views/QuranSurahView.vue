93fa6926e36be5160027becf211e3c3c03469063
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2025-04-21 23:21:08 +0200

    fix: update QuranSurahView to correctly handle ayat data and display Basmala

--- src/views/QuranSurahView.vue
+++ src/views/QuranSurahView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { computed } from 'vue'
 import { RouterLink } from 'vue-router'
 import { IconChevronLeft } from '@tabler/icons-vue'
 import { useFetch } from '@vueuse/core'
@@ -11,6 +12,27 @@ import ErrorState from '@/components/ErrorState.vue'
 
 const surahId = useRouteParams('surah')
 const { isFetching, data: surah, error } = useFetch(`https://api.alquran.cloud/v1/surah/${surahId.value}`).json().get()
+
+// Prepare the ayat data
+const ayat = computed(() => {
+  if (surah.value) {
+    let ayat = surah.value.data.ayahs
+
+    // Remove the Basmala from the Al-Fatiha
+    if (surah.value.data.number === 1) {
+      ayat = ayat.slice(1)
+    }
+
+    return ayat.map((ayah) => ({
+      ...ayah,
+
+      // Remove the Basmala from the first Ayah of other Surahs
+      text: ayah.numberInSurah === 1 ? ayah.text.replace('بِسۡمِ ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ', '') : ayah.text,
+    }))
+  }
+
+  return []
+})
 </script>
 
 <template>
@@ -29,10 +51,12 @@ const { isFetching, data: surah, error } = useFetch(`https://api.alquran.cloud/v
       :subtitle="`عدد الآيات: ${surah.data.numberOfAyahs} - ${surah.data.revelationType === 'Meccan' ? 'مكية' : 'مدنية'}`"
     />
 
-    <div class="ayat mb-4">
-      <template v-for="ayah in surah.data.ayahs" :key="ayah.number">
-        <span class="ayah font-quran">{{ ayah.text }}</span>
-        <span class="ayah-number font-quran">﴿{{ ayah.numberInSurah }}﴾</span>
+    <div class="ayat font-quran mb-4">
+      <span class="basmallah">بِسْمِ ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ</span>
+
+      <template v-for="ayah in ayat" :key="ayah.number">
+        <span class="ayah">{{ ayah.text }}</span>
+        <span class="ayah-number">﴿{{ ayah.numberInSurah }}﴾</span>
       </template>
     </div>
 
@@ -57,7 +81,16 @@ const { isFetching, data: surah, error } = useFetch(`https://api.alquran.cloud/v
     text-align: justify;
     border: 1px solid var(--bs-border-color);
 
-    span {
+    .basmallah {
+      display: block;
+      font-size: 2rem;
+      line-height: 2.5;
+      text-align: center;
+      margin-bottom: 1rem;
+    }
+
+    .ayah,
+    .ayah-number {
       line-height: 2.125;
       font-size: 1.5rem;
       margin-bottom: 1rem;
@@ -65,7 +98,6 @@ const { isFetching, data: surah, error } = useFetch(`https://api.alquran.cloud/v
 
     .ayah-number {
       padding: 0.25rem 0.5rem;
-      padding-inline-start: 0;
       color: var(--bs-gray-600);
       vertical-align: middle;
     }
