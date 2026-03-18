26e691893744ef21d17ab207892c1c94bd8d0db3
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2026-03-18 07:51:39 +0200

    Add icon prop to ErrorState component and integrate it in EmbedComponentView for enhanced error display

--- src/components/ErrorState.vue
+++ src/components/ErrorState.vue
@@ -5,6 +5,10 @@ defineProps({
   code: {
     type: Number,
   },
+  icon: {
+    type: [Object, Function],
+    default: null,
+  },
   message: {
     type: String,
     default: 'حدث خطأ، برجاء المحاولة في وقت لاحق.',
@@ -16,7 +20,8 @@ defineProps({
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-lg-6 text-center">
-        <h1 class="display-1">{{ toArabicNumerals(code) }}</h1>
+        <component v-if="icon" :is="icon" size="3rem" class="mb-3" />
+        <h1 v-if="code" class="display-1">{{ toArabicNumerals(code) }}</h1>
         <p class="lead">{{ message }}</p>
       </div>
     </div>
--- src/views/EmbedComponentView.vue
+++ src/views/EmbedComponentView.vue
@@ -3,6 +3,8 @@ import { computed, defineAsyncComponent } from 'vue'
 import { useRoute } from 'vue-router'
 import Heading from '@/components/Heading.vue'
 import Page from '@/components/Layout/Page.vue'
+import ErrorState from '@/components/ErrorState.vue'
+import { IconAlertTriangle } from '@tabler/icons-vue'
 
 const components = {
   'prayer-times': defineAsyncComponent(() => import('@/components/PrayerTimes.vue')),
@@ -31,7 +33,11 @@ const params = computed(() => {
 
 <template>
   <Page>
-    <Heading v-if="params.title" :size="2" class="mb-4" :title="params.title" :subtitle="params.subtitle" />
-    <component v-if="component" :is="component" />
+    <template v-if="component">
+      <Heading v-if="params.title" :size="2" class="mb-4" :title="params.title" :subtitle="params.subtitle" />
+      <component :is="component" />
+    </template>
+
+    <ErrorState v-else :icon="IconAlertTriangle" message="خطأ في الإعدادات: المكوّن غير موجود." />
   </Page>
 </template>
