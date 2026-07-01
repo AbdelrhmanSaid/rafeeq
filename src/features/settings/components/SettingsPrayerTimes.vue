7a11184d581418a2339955708581410b0c1aed87
Abdelrhman Said <70618755+AbdelrhmanSaid@users.noreply.github.com>
2026-07-01 11:43:11 +0300

    Update SettingsPrayerTimes.vue

--- src/features/settings/components/SettingsPrayerTimes.vue
+++ src/features/settings/components/SettingsPrayerTimes.vue
@@ -29,24 +29,7 @@ const location = computed(() => {
 
 <template>
   <SettingsSection title="مواقيت الصلاة" description="حدّد موقعك وطريقة عرض المواقيت" :icon="IconClockHour4">
-    <div class="mb-3">
-      <div class="input-group">
-        <div class="form-floating">
-          <input id="location" type="text" class="form-control" :value="location" readonly />
-          <label for="location">الموقع</label>
-        </div>
-
-        <button type="button" class="input-group-text" @click="detect">
-          <IconRefreshDot size="1.25rem" />
-        </button>
-
-        <button type="button" class="input-group-text" @click="store.clear">
-          <IconTrash size="1.25rem" />
-        </button>
-      </div>
-    </div>
-
-    <div class="mb-3">
+<div class="mb-3">
       <span class="d-block mb-2">طريقة العرض</span>
       <div class="btn-group-toggle">
         <button class="btn-toggle" :class="{ active: store.layout === 'cards' }" @click="store.layout = 'cards'">
@@ -60,6 +43,23 @@ const location = computed(() => {
         <button class="btn-toggle" :class="{ active: store.layout === 'auto' }" @click="store.layout = 'auto'">
           <IconDevices :size="16" />
           <span>تلقائي</span>
+        </button>
+      </div>
+    </div>
+
+    <div class="mb-3">
+      <div class="input-group">
+        <div class="form-floating">
+          <input id="location" type="text" class="form-control" :value="location" readonly />
+          <label for="location">الموقع</label>
+        </div>
+
+        <button type="button" class="input-group-text" @click="detect">
+          <IconRefreshDot size="1.25rem" />
+        </button>
+
+        <button type="button" class="input-group-text" @click="store.clear">
+          <IconTrash size="1.25rem" />
         </button>
       </div>
     </div>
